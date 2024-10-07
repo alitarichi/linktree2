c7742da7fcf66e6601789a16bcd2b26feb1961fd
ali tarichi <tarichiali@gmail.com>
2024-10-07 18:48:10 +0300

    fixed language and removed some unneccesary files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
-import Header from "../components/header";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/modeToggle";
+
+const useLanguageToggle = () => {
+  const [isEnglish, setIsEnglish] = useState(true);
+
+  const toggleLanguage = () => {
+    setIsEnglish((prev) => !prev);
+  };
+
+  return { isEnglish, toggleLanguage };
+};
 
 export default function Page() {
+  const { isEnglish, toggleLanguage } = useLanguageToggle();
   return (
     <>
-      <div className="items-center flex justify-center">
-        <Header />
+      <div className="items-center flex justify-end p-16 gap-16 ">
+        <ModeToggle />
+        <button
+          onClick={toggleLanguage}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          {isEnglish ? "Farsi 🇮🇷" : "English 🇺🇸"}
+        </button>
       </div>
       <div className="flex-row items-center text-center">
         <div className="flex items-center justify-center pb-8">
@@ -18,8 +38,10 @@ export default function Page() {
             className="flex items-center justify-center"
           />
         </div>
-        <h1 className="text-2xl font-bold">DOMION</h1>
-        <h2 className="text-lg">Robot Influencer</h2>
+        <h1 className="text-2xl font-bold">
+          {isEnglish ? "Your Name" : "نام"}
+        </h1>
+        <h2 className="text-lg">Influencer</h2>
 
         <div className="items-center justify-center flex gap-4 pt-8">
           <Image src="/facebook2.png" alt="facebook" width={32} height={32} />
@@ -33,21 +55,21 @@ export default function Page() {
             className="flex gap-2 items-center justify-center 
     mx-auto w-full mt-8 px-8  md:max-w-3xl sm:max-w-xl"
           >
-            Website
+            {isEnglish ? "Website" : "وب‌ سایت"}
             <Image src="/internet.png" alt="telegram" width={24} height={24} />
           </Button>
           <Button
             className="flex gap-2 items-center justify-center 
     mx-auto w-full mt-8 px-8  md:max-w-3xl sm:max-w-xl"
           >
-            Address
+            {isEnglish ? "Address" : "آدرس"}
             <Image src="/location.png" alt="telegram" width={24} height={24} />
           </Button>
           <Button
             className="flex gap-2 items-center justify-center 
     mx-auto w-full mt-8 px-8  md:max-w-3xl sm:max-w-xl"
           >
-            Contact Us
+            {isEnglish ? "Contact Us" : "با ما تماس بگیرید"}
             <Image
               src="/phone-call.png"
               alt="telegram"
@@ -59,14 +81,14 @@ export default function Page() {
             className="flex gap-2 items-center justify-center 
     mx-auto w-full mt-8 px-8  md:max-w-3xl sm:max-w-xl"
           >
-            Chat via Telegram
+            {isEnglish ? "Chat via Telegram" : "چت از طریق تلگرام"}
             <Image src="/telegram.png" alt="telegram" width={24} height={24} />
           </Button>
           <Button
             className="flex gap-2 items-center justify-center 
     mx-auto w-full mt-8 px-8  md:max-w-3xl sm:max-w-xl"
           >
-            Chat via Signal
+            {isEnglish ? "Chat via Signal" : "چت از طریق سیگنال"}
             <Image src="/signal.png" alt="telegram" width={24} height={24} />
           </Button>
         </div>
